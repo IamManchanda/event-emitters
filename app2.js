858a4c2d2d482dec6fa1556a447bfa830e8212cc
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-02-23 17:54:30 +0530

    Class keyword and Old school solution for prototypal inheritance!

--- app2.js
+++ app2.js
@@ -1,23 +1,26 @@
 /**
- * Node.js
+ * Node.js - ES5 Version
  */
 
-const EventEmitter = require('events');
-const util = require('util');
+var EventEmitter = require('events');
+var util = require('util');
 
 function Message() {
-  this.message = 'Hey dude';
+  this.name = 'Harry Manchanda';
+  this.message = 'Hey Dude';
 }
 
-util.inherits(Message, EventEmitter);
+util.inherits(Message, EventEmitter)
 
-Message.prototype.showMessage =  function() {
-  this.emit('showIt', this.message);
-};
+Message.prototype.showMessage = function() {
+  this.emit('showIt', this.message + ' ' + this.name);
+}
 
-const message = new Message();
+var message = new Message();
+
 message.on('showIt', function(data) {
   console.log(data);
 });
 
 message.showMessage();
+// => Hey Dude Harry Manchanda
--- app3.js
+++ app3.js
@@ -1,16 +1,19 @@
 /**
- * Node.js
+ * Node.js - ES6+ Version
  */
 
 const EventEmitter = require('events');
+
 class Message extends EventEmitter {
   constructor() {
     super();
-    this.message = 'Hey dude';
-    this.showMessage = () => this.emit('showIt', this.message);
+    this.name = 'Harry Manchanda';
+    this.message = 'Hey Dude';
+    this.showMessage = () => this.emit('showIt', `${this.message} ${this.name}`);
   }
 }
 
 const message = new Message();
 message.on('showIt', (data) => console.log(data));
 message.showMessage();
+// => Hey Dude Harry Manchanda
