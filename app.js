323622c78df9d56a29e102db6a578e9b5f121f7a
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-02-23 13:23:24 +0530

    Make code easy to reason about!

--- app.js
+++ app.js
@@ -5,16 +5,16 @@
 const events = require('events');
 const eventEmitter = new events.EventEmitter();
 
-const ringBell = () => {
+// Questions
+eventEmitter.on('guestHere', () => {
   console.log('ring ring ring');
-};
-eventEmitter.on('guestHere', ringBell);
+});
 
-const sayHello = () => {
+eventEmitter.on('guestHere', () => {
   console.log(`Hello who's there`);
-};
-eventEmitter.on('guestHere', sayHello);
+});
 
+// Answers 
 eventEmitter.on('guestHere', (action) => {
   console.log(action);
 });
